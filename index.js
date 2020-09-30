00b844ce66b26f90162dd3820b7251e414702e6a
Griffith <Mark.Griffith@Mark.Griffith>
2020-09-30 14:42:46 -0400

    feat:add example for api request.

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 // index.js
+const fetch = require('cross-fetch');
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
+
+async function imageFetch() {
+    let image = await fetch("https://source.unsplash.com/random/400x400")
+    return image.url
+  }
+
 /**
   * DATA is the object that contains all
   * the data to be provided to Mustache
   * Notice the "name" and "date" property.
 */
-let DATA = {
-  name: 'Mark Griffith',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'America/New_York',
-  }),
-};
+async function buildData(){
+    let image = await imageFetch()
+    return DATA = {
+    name: 'Mark Griffith',
+    date: new Date().toLocaleDateString('en-US', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'short',
+        timeZone: 'America/New_York',
+    }),
+    picture: image
+    };
+}
+ 
 /**
   * A - We open 'main.mustache'
   * B - We ask Mustache to render our file with the data
   * C - We create a README.md file with the generated output
   */
-function generateReadMe() {
+ async function generateReadMe() {
+  const DATA = await buildData()
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
     if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
+    const output =  Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
-<h2> Hi, I'm Mark Griffith and it is currently Monday, 28 September, 15:46 GMT-4.<img src="https://toggl.com/blog/wp-content/uploads/2018/08/programming-comic-blog-header.png" width="50"></h2>
+<h2> Hi, I'm Mark Griffith and it is currently Wednesday, September 30, 2:41 PM EDT.<img src="https://toggl.com/blog/wp-content/uploads/2018/08/programming-comic-blog-header.png" width="50"></h2>
 <img align='right' src="https://media.giphy.com/media/LcfBYS8BKhCvK/giphy.gif" width="230">
 <p><em>Software Engineer at <a href="https://www.weightwatchers.com/us/about-WW">WW International</a><img src="https://media.giphy.com/media/JrXc72Pz2Ib1dBK13T/giphy.gif" width="30" 
 </em></p>
+### Image of the hour
+<img src="https://images.unsplash.com/photo-1600087626014-e652e18bbff2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=400">
 
 ### More on me below... <img src="https://media.giphy.com/media/VgCDAzcKvsR6OM0uWg/giphy.gif" width="50"> 
 
