7e8e188e0caf1d0ca4468ca1e04c256bd3224e77
Mark Griffith <70962504+mgriffith-ww@users.noreply.github.com>
2020-09-30 15:01:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
 async function imageFetch() {
-    let image = await fetch("https://source.unsplash.com/random/200x200")
+    let image = await fetch("https://source.unsplash.com/random/200x300")
     return image.url
   }
 
